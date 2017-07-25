c43b4415d79e64d0113125c1ec46217777c0b252
Catsayerbleble <2103088079@qq.com>
2017-07-25 17:30:24 +0800

    去除控制台打印

--- widget/script/wolf.js
+++ widget/script/wolf.js
@@ -1,11 +1,11 @@
 /**
- * 请在api.js之后引入 
+ * 请在api.js之后引入
  */
 
 function setTopBar() {
 	var systemType = api.systemType;
 		$(".back").css("top", "23px");
-	
+
 			$(".content").css("marginTop", "60px");
 
 	$api.fixStatusBar($api.dom('header'));
