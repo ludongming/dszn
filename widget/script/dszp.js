d1b186e4b638671839446b8dd895b1c33b232732
wolf <116311316@qq.com>
2017-08-29 11:07:27 +0800

    顶上宅配下载图片金豆

--- widget/script/dszp.js
+++ widget/script/dszp.js
@@ -24,5 +24,64 @@ function bigImage(image,index) {
 
       photoBrowser.close();
     }
+
+
+    if (ret.eventType == "longPress") {
+      photoBrowser.getImage({
+          index: 0
+      }, function(ret, err) {
+          if (ret) {
+              saveImage(ret.path);
+          } else {
+              alert(JSON.stringify(err));
+          }
+      });
+
+   }
+
   });
+}
+
+
+function saveImage(image) {
+    api.confirm({
+        title: '提示',
+        msg: '保存图片到手机 20金豆',
+        buttons: ['确定', '取消']
+    }, function(ret, err) {
+        var index = ret.buttonIndex;
+        if (index == 1) {
+saveImageGallery(image);
+        }
+    });
+
+}
+
+function saveImageGallery(image){
+
+  api.ajax({
+      url: 'http://www.d-shang.com/index.php?app/downloadpic/?openid='+OPENID,
+  },function(ret, err){
+      if (ret.status) {
+
+        api.saveMediaToAlbum({
+            path: image
+        }, function(ret, err) {
+            if (ret && ret.status) {
+                api.toast({
+                    msg: '保存成功',
+                    duration: 1000,
+                    location: 'bottom'
+                });
+            } else {
+                alert('保存失败');
+            }
+        });
+
+
+      } else {
+        alert( ret.message);
+      }
+  });
+
 }
