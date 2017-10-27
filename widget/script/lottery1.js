0b3accc63a71cceaf0b29d63739765ca7e56998f
Catsayerbleble <2103088079@qq.com>
2017-10-27 11:01:40 +0800

    抽奖

--- widget/script/lottery1.js
+++ widget/script/lottery1.js
@@ -56,31 +56,31 @@ var lottery={
 							lottery.prize="狮子靠枕";
 						}
 						else if(lottery.prize==2){
-							lottery.prize="F3取暖器";
+							lottery.prize="再接再厉";
 						}
 						else if(lottery.prize==3){
-							lottery.prize="毛巾一条";
+							lottery.prize="F3取暖器";
 						}
 						else if(lottery.prize==4){
-							lottery.prize="万能拖把一个";
+							lottery.prize="再接再厉";
 						}
 						else if(lottery.prize==5){
-							lottery.prize="谢谢惠顾";
+							lottery.prize="太阳炉";
 						}
 						else if(lottery.prize==8){
-							lottery.prize="抱枕被一条";
+							lottery.prize="再接再厉";
 						}
 						else if(lottery.prize==9){
-							lottery.prize="毛巾一条";
+							lottery.prize="LED方灯";
 						}
 						else if(lottery.prize==10){
-							lottery.prize="谢谢惠顾";
+							lottery.prize="再接再厉";
 						}
 						else if(lottery.prize==7){
-							lottery.prize="再接再励";
+							lottery.prize="狮子靠枕";
 						}
 						else if(lottery.prize==6){
-							lottery.prize="毛巾一条";
+							lottery.prize="再接再厉";
 						}
 			alert(lottery.prize);
 						lottery.btn.show();
